42d2fd04e60f73a659960549088eeef4e99d5e8a
zhihui1532450 <zhihui153245@outlook.com>
2023-12-25 23:16:30 +0800

    first commit

---
+++ src/pages/_app.jsx
@@ -0,0 +1,26 @@
+import 'focus-visible'
+import Link from 'next/link'
+import '@/styles/tailwind.css'
+// import { ClerkProvider, SignedIn, SignedOut } from '@clerk/nextjs'
+
+// export default function App({ Component, pageProps }) {
+//   return (
+//     <ClerkProvider>
+//       <SignedIn>
+//         <Component {...pageProps} />
+//       </SignedIn>
+//       <SignedOut>
+//         <Component {...pageProps} />
+//       </SignedOut>
+//     </ClerkProvider>
+//   )
+// }
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
